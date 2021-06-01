c97ba359a40217ccad049df59a25475efb791fbe
Nickshe <62675720+Nickshe@users.noreply.github.com>
2021-06-02 00:17:56 +0300

    Add files via upload

---
+++ vectorspisok/MyList.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAOD_2
+{
+    class MyList<T> : IEnumerable
+    {
+        static int size = 0;
+        static T[] array = new T[size];
+        static int cop = 1;
+
+        public IEnumerator GetEnumerator()
+        {
+            if (size > 0)
+            {
+                for (int i = 0; i < size; i++)
+                    yield return array[i];
+            }
+        }
+
+        public void Add(T item)
+        {
+            size += 1;
+            if (size < cop)
+            {
+                array[size - 1] = item;
+            }
+            else
+            {
+                cop = cop * 2;
+                Array.Resize<T>(ref array, cop);
+                array[size - 1] = item;
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index >= size)
+            {
+                Add(item);
+            }
+            else
+            {
+                T temp = array[0];
+                if (size >= cop)
+                {
+                    cop = cop * 2;
+                    Array.Resize<T>(ref array, cop);
+                }
+                size += 1;
+                array[size] = array[0];
+                for (int i = index; i < size; i++)
+                {
+                    temp = array[i + 1];
+                    array[i + 1] = array[index];
+                    array[index] = temp;
+                }
+                array[index] = item;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            T temp = array[0];
+            for (int i = size; i > index; i--)
+            {
+                temp = array[i - 1];
+                array[i - 1] = array[index];
+                array[index] = temp;
+            }
+            size--;
+        }
+
+        public T Last
+        {
+            get { return array[size - 1]; }
+            set { array[size - 1] = value; }
+
+        }
+
+        public T First
+        {
+            get { return array[0]; }
+            set { array[0] = value; }
+        }
+
+        public void Clear()
+        {
+            Array.Resize<T>(ref array, 0);
+            size = 0;
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (var i in array)
+            {
+                if (item.Equals(i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                action(array[i]);
+            }
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (match(array[i]))
+                {
+                    return array[i];
+                }
+            }
+            return default(T);
+        }
+
+        public int FindIndex(Predicate<T> match)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (match(array[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+
+    }
+}
+
---
+++ vectorspisok/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAOD_2
+{
+    class Program
+    {
+        public static MyList<int> VectorList = new MyList<int>();
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                VectorList.Add(rand.Next(1, 100));
+                Console.Write(VectorList[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Insert(Добавляем нолики в начало, в конец и в середину)");
+            VectorList.Insert(0, 0);
+            VectorList.Insert(5, 0);
+            VectorList.Insert(VectorList.Size, 0);
+            foreach (var v in VectorList)
+            {
+                Console.Write(v + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("RemoveAt(Удаляем наши нолики)");
+            VectorList.RemoveAt(0);
+            VectorList.RemoveAt(4);
+            VectorList.RemoveAt(VectorList.Size);
+            foreach (var v in VectorList)
+            {
+                Console.Write(v + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Contains");
+            Console.WriteLine(VectorList.Contains(VectorList[0]));
+            Console.WriteLine(VectorList.Contains(1000));
+            Console.WriteLine("First");
+            Console.WriteLine(VectorList.First);
+            Console.WriteLine("Last");
+            Console.WriteLine(VectorList.Last);
+            Console.WriteLine("Find(Число кратное трём)");
+            Console.WriteLine(VectorList.Find(x => { return x % 3 == 0; }));
+            Console.WriteLine("FindIndex(Индекс этого числа)");
+            Console.WriteLine(VectorList.FindIndex(x => { return x % 3 == 0; }));
+            Console.WriteLine("ForEach");
+            VectorList.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+            Console.WriteLine("Clear");
+            VectorList.Clear();
+            foreach (var v in VectorList)
+            {
+                Console.Write(v + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
